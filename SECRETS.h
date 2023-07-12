2e1fd0161b1c5ff1c31c2a666cdc7feb98b31b49
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-07-12 11:26:46 +0200

    Create SECRETS.h

---
+++ SECRETS.h
@@ -0,0 +1,16 @@
+// SECRETS.h
+// MIT License
+// by Lucas Placentino
+
+// Eufy Cam Controller - ESP32
+
+// ---- Secrets ----
+#ifndef SECRETS_h
+#define SECRETS_h
+
+#define eufy_email "mymail@example.org"
+#define eufy_password "mysecurepassword"
+#define api_key "myeufyapikey"
+//...
+
+#endif
